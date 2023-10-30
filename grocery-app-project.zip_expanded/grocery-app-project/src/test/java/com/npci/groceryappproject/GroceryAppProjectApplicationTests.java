0edf4baea111c23666a610e981327289b81677d1
AshwiniKale01 <ashukale1201@gmail.com>
2023-10-30 04:57:02 -0700

    Project Initialize

---
+++ grocery-app-project.zip_expanded/grocery-app-project/src/test/java/com/npci/groceryappproject/GroceryAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npci.groceryappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
